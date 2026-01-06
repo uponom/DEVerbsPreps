67cccee19ba5e87e7cb32e2e532eb0b3af6ff248
Yurii Ponomarenko <yurapo@live.com>
2026-01-06 23:01:54 +0100

    Bump app version to v4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v3';
+const CACHE_VERSION = 'v4';
 const CACHE_NAME = `DEVerbsPreps-cache-${CACHE_VERSION}`;
 const urlsToCache = [
   '/DEVerbsPreps/', // Важно, если ваша страница доступна по корневому URL
